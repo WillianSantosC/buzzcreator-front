c382c4175bb8851c17e6bfd528a673fe10cdca81
Willian Santos <willian.s@develops.today>
2025-05-21 07:58:39 -0300

    feat: added function to fetch data from backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import { items as booksMock } from "@/components/BookCard/mock";
 import Home, { BooksTemplateProps } from "@/pages/Home";
 
-function getHomePageData() {
-  const output = { books: booksMock };
+async function getHomePageData(): Promise<BooksTemplateProps> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/book`, {
+      cache: "no-store",
+    });
 
-  return output;
+    if (!res.ok) {
+      return { books: [] };
+    }
+
+    const books = await res.json();
+    return { books };
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return { books: [] };
+  }
 }
 
-export default function Index() {
-  const props: BooksTemplateProps = getHomePageData();
-
+export default async function Index() {
+  const props = await getHomePageData();
   return <Home {...props} />;
 }
