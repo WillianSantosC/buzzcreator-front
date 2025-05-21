cb1848c812b350fd5a445ec7d9c5b789a46af139
Willian Santos <willian.s@develops.today>
2025-05-21 15:28:43 -0300

    refactor: remove unused items

--- src/pages/Home/mock.tsx
+++
@@ -1,4 +0,0 @@
-import { items as filterItemsMock } from "@/components/ExploreSidebar/mock";
-import { items as gamesMock } from "@/components/GameCardSlider/mock";
-
-export const items = { games: gamesMock, filterItems: filterItemsMock };
